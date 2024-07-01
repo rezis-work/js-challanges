11fca9b66e0323818f6b0ae82c932cdb9dbc7e9d
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-07-01 23:21:26 +0400

    implement chunk challange

---
+++ chunk.js
@@ -0,0 +1,14 @@
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    const chunk = array.slice(index, index + size);
+    chunked.push(chunk);
+    index += size;
+  }
+
+  return chunked;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6], 3));
