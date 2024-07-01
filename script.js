58e6628197b41429b3c406ba7a371a0bd3517e3e
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-07-01 21:09:01 +0400

    implement capitalize challange

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function reverseInt(num) {
-  let reversedNum = String(num).split("").reverse().join("");
-  return parseInt(reversedNum) * Math.sign(num);
+// to lowercase everything
+
+// solution 1
+
+function toCapitalize(str) {
+  let capitalizedLetter = [];
+  let lowerStr = str.toLowerCase().split(" ");
+  for (let i = 0; i < lowerStr.length; i++) {
+    capitalizedLetter.push(
+      lowerStr[i].slice(0, 1).toUpperCase() + lowerStr[i].slice(1)
+    );
+  }
+  return capitalizedLetter.join(" ");
 }
 
-console.log(reverseInt(-1234));
+console.log(toCapitalize("heLlo woRLd how are you"));
+
+// solution 2
+
+function capitalize(letter) {
+  return letter
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+console.log(capitalize("how aRe you"));
