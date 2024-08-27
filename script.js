58e5d3bfd5d5c5f8e2c9051cbff8191cfebebcfb
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-08-27 21:27:24 +0400

    add multiple link list methods

---
+++ script.js
@@ -0,0 +1,89 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+
+    let temp = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    temp.prev = null;
+
+    this.length--;
+    return temp;
+  }
+
+  unshift(value) {
+    let newNode = new Node(value);
+
+    if (this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+
+    let temp = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    this.head = temp.next;
+    this.head.prev = null;
+    temp.next = null;
+
+    this.length--;
+    return temp;
+  }
+}
+
+const myDoubleLinkList = new DoublyLinkList(0);
+myDoubleLinkList.push(1);
+myDoubleLinkList.push(2);
+myDoubleLinkList.push(3);
+myDoubleLinkList.pop();
+myDoubleLinkList.shift();
+console.log(myDoubleLinkList);
